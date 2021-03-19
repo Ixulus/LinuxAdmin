cb35802c8b331805935eade3494d631332885af9
Ixulus <75646462+Ixulus@users.noreply.github.com>
2021-03-19 19:38:20 -0400

    Update NetDump.sh

--- NetDump.sh
+++ NetDump.sh
@@ -5,8 +5,8 @@ sed -n 's/        //p' net2.txt >> net3.txt       # This removes the empty space
 grep "nameserver" /etc/resolv.conf >> dns.txt     # This gets DNS server info
 sed -n '1,3p' dns.txt >> dns2.txt                 # Removes extra DNS info
 cat net3.txt dns2.txt >> ipdns.txt                # Combine net info and DNS info
-mv ipdns.txt NetInfo$(date +%Y_%m_%d_%H_%M_%S).txt
-mv NetInfo$(date +%Y_%m_%d_%H_%M_%S).txt /home/karnold/ServerInfo/
+mv ipdns.txt NetInfo$(date +%Y_%m_%d_%H_%M_%S).txt # Appead a timestamp to the file
+mv NetInfo$(date +%Y_%m_%d_%H_%M_%S).txt /home/karnold/ServerInfo/ # Move file to new location
 						  # Rename file, append timestamp to end
                                                   # The rest is removing excess files
 rm net1.txt
